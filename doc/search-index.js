1078b73444103fa23725d4787028b75dbaa1d49d
DoumanAsh <custparasite@gmx.se>
2015-06-15 09:09:19 +0300

    rusty-cute-macro page initial

---
+++ doc/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['rusty_cute_macros'] = {"items":[[0,"","rusty_cute_macros","A crate with bunch of lazy macros",null,null],[14,"cmd!","","Macro to construct subprocess command",null,null],[14,"exec_silent_cmd!","","Macro to exectute subprocess silently.",null,null],[14,"is_file!","","Macro to check if given path/string belongs to file.",null,null],[14,"is_dir!","","Macro to check if given path/string belongs to directory.",null,null],[14,"trace!","","Trace macro like ```println```",null,null]],"paths":[]};
+initSearch(searchIndex);
